3d3a172b7551173eff6ab07f1449323ae43bb0b1
Adrier José <80015771+adrierjs@users.noreply.github.com>
2022-08-13 03:32:18 -0300

    Update Questao2.py

--- Questao2.py
+++ Questao2.py
@@ -1,27 +1,22 @@
-def verificar(array):
-  for i in range(len(array)):
-    if array[i] == n:
-      return True
+def verificar(n):
+  t1 = 0
+  t2 = 1
+  cont = 3 
+  while cont <= n:
+      t3 = t1 + t2
+      if t3 == n:
+        return True
+      t1 = t2
+      t2 = t3
+      cont +=1
   return False
   
-  
-
 n = int(input("Digite um número: "))
-t1 = 0
-t2 = 1
-cont = 3 
-array = []
-while cont <= n:
-    t3 = t1 + t2
-    array.append(t3)
-    t1 = t2
-    t2 = t3
-    cont +=1
 
-if n == 1 or n ==2:
+if n == 1 or n ==2 or n ==3 or n ==5:
   print("O número pertence a sequência de Fibonacci")
 else:
-  retorno = verificar(array)
+  retorno = verificar(n)
   if retorno:
     print("O número pertence a sequência de Fibonacci")
   else:
